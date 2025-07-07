c5217e3e408d59aa3220f5f28134be9e80cba724
Gera.Mojesh <geramojesh26@gmail.com>
2025-07-07 07:07:23 +0530

    N.py
    Working

---
+++ N.py
@@ -0,0 +1,4 @@
+//4.	Write a python program to find the sum of first n natural numbers. //
+n = int(input("Enter a natural number: "))
+sum_natural_numbers = n * (n + 1) // 2
+print(f"The sum of the first {n} natural numbers is: {sum_natural_numbers}")
