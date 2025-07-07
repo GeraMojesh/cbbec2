a06c09c93abee357c247c3ff3c7c2ccba7245d96
Gera.Mojesh <geramojesh26@gmail.com>
2025-07-07 07:02:35 +0530

    PrNrZ.py
    Working

--- PrNrZ.py
+++ PrNrZ.py
@@ -1,3 +1,4 @@
+//1.Write a python program to check if the number is positive or negative or zero and display an appropriate message. //
 number = float(input("Enter a number: "))
 if number > 0:
     print("The number is positive.")
